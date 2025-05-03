0d2193344ff0c085c3d1b686c60e157f14ce4072
dev-patrick-alunya <patrickalunya106@gmail.com>
2025-05-03 22:37:53 +0300

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,17 @@ document.getElementById('hamburger-menu').addEventListener('click', function() {
             navLinks.classList.toggle('active');
         });
 
-        let borderSize = 2; 
-        const maxBorderSize = 3; 
-        const borderIncrement = 1; 
-        const whatsappIcon = document.getElementById('wa-icon');
+        // let borderSize = 2; 
+        // const maxBorderSize = 3; 
+        // const borderIncrement = 1; 
+        // const whatsappIcon = document.getElementById('wa-icon');
         
-        setInterval(() => {
-            borderSize += borderIncrement;
-            if (borderSize > maxBorderSize) {
-                borderSize = 2; 
-            }
-            whatsappIcon.style.borderWidth = `${borderSize}px`;
-        }, 500);
+        // setInterval(() => {
+        //     borderSize += borderIncrement;
+        //     if (borderSize > maxBorderSize) {
+        //         borderSize = 2; 
+        //     }
+        //     whatsappIcon.style.borderWidth = `${borderSize}px`;
+        // }, 500);
 
          
